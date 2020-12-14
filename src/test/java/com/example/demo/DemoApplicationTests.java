d45b31254148a95131dda165533af6155488a9c8
guanghui_qi <372567216@qq.com>
2020-12-14 22:24:19 +0800

    集成单元测试

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,43 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
+
+import com.example.demo.entity.Params;
+import com.example.demo.repository.ParamsRepository;
+
+import java.util.Objects;
+import java.util.Random;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest(classes = DemoApplication.class)
+@RunWith(SpringRunner.class)
+public class DemoApplicationTests {
+
+	@Autowired
+	RedisTemplate redisTemplate;
 
 	@Test
-	void contextLoads() {
+	public void testRedis() {
+		redisTemplate.opsForValue().set("ok", "test");
+		assert Objects.nonNull(redisTemplate.opsForValue().get("ok"));
+	}
+
+	@Autowired
+	ParamsRepository paramsRepository;
+
+	@Test
+	public void testInsert() {
+		Random random = new Random();
+		Params params = new Params();
+		params.setParam1("id1-" + random.nextInt(100));
+		params.setParam2("id2-" + random.nextInt(100));
+		assert Objects.nonNull( paramsRepository.save(params));
 	}
 
 }
